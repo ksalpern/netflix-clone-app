6ccc4397360ba3d417065a7ffa7f0a80465775d2
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-07 14:55:15 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAcOSGBZMzClypUQUQyp59WJqTY9cIauJU",
+  authDomain: "netflix-clone-app-c2822.firebaseapp.com",
+  projectId: "netflix-clone-app-c2822",
+  storageBucket: "netflix-clone-app-c2822.appspot.com",
+  messagingSenderId: "634726367412",
+  appId: "1:634726367412:web:1bd44b16c377a9ddce17e1",
+};
