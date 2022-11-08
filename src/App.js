3043b6715a51834969aa2f8e796d20d0b5bd48af
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-08 08:47:52 +0200

    add nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Homescreen from "./components/Homescreen";
 
 function App() {
   return (
     <div>
-      <h1>Let's build Netflix</h1>
+      <Homescreen/>
     </div>
   );
 }
---
+++ src/components/Homescreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Homescreen.css";
+import Nav from "./Nav";
+
+function Homescreen() {
+  return (
+    <div className="homescreen">
+      {/* Nav */}
+      <Nav />
+
+      {/* Banner */}
+
+      {/* Row */}
+    </div>
+  );
+}
+
+export default Homescreen;
