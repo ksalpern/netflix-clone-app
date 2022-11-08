c02e56928dd388cd329e642ee508fa97d3cdcf8d
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-08 12:16:07 +0200

    remove default styles & add Banner component

--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Banner from "./Banner";
 import "./Homescreen.css";
 import Nav from "./Nav";
 
@@ -9,6 +10,7 @@ function Homescreen() {
       <Nav />
 
       {/* Banner */}
+      <Banner />
 
       {/* Row */}
     </div>
