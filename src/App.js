517b66a047d7221ab5c130a0513f17650c363002
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-08 20:08:35 +0200

    add Row component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Homescreen from "./components/Homescreen";
 
 function App() {
   return (
-    <div>
+    <div className="app">
       <Homescreen/>
     </div>
   );
--- src/components/Homescreen.js
+++ src/components/Homescreen.js
@@ -1,7 +1,9 @@
 import React from "react";
+import requests from "../features/Requests";
 import Banner from "./Banner";
 import "./Homescreen.css";
 import Nav from "./Nav";
+import Row from "./Row";
 
 function Homescreen() {
   return (
@@ -13,6 +15,23 @@ function Homescreen() {
       <Banner />
 
       {/* Row */}
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action And Adventure Series"
+        fetchUrl={requests.fetchActionAndAdventureSeries}
+      />
+      <Row title="Comedy Series" fetchUrl={requests.fetchComedySeries} />
+      <Row title="Reality Series" fetchUrl={requests.fetchRealitySeries} />
+      <Row
+        title="Documentary Series"
+        fetchUrl={requests.fetchDocumentarySeries}
+      />
     </div>
   );
 }
