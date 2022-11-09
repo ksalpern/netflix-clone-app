4e53b85d046ce1c7d3ef59d5e091c9c96b2f4c14
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-09 12:02:30 +0200

    update firestore config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
 const firebaseConfig = {
   apiKey: "AIzaSyAcOSGBZMzClypUQUQyp59WJqTY9cIauJU",
   authDomain: "netflix-clone-app-c2822.firebaseapp.com",
@@ -6,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "634726367412",
   appId: "1:634726367412:web:1bd44b16c377a9ddce17e1",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
