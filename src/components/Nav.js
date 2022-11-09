c841d462c5f7641456f8eb9a44cdab9cba9b32bb
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-09 13:43:58 +0200

    aadd navigation to home

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -23,6 +23,7 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__content">
         <img
+          onClick={() => histoty("/")}
           className="nav__logo"
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
