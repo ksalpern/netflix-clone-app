138686fe9f191d2d4f03883677fe10843ad2198e
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-09 09:25:44 +0200

    add signIn screen onClick

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
+import SignIn from "./SignIn";
 
 function Login() {
   const [signIn, setSignIn] = useState(false);
---
+++ src/components/SignIn.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./SignIn.css";
+
+function SignIn() {
+  return <div className="sign">
+    
+  </div>;
+}
+
+export default SignIn;
