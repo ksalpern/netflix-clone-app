4064df0fa40a9e50c3cff61e69afcf930fab90ca
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-09 09:26:05 +0200

    render Login component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Homescreen from "./components/Homescreen";
+import Login from "./components/Login";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<Homescreen />} />
-        </Routes>
+        {!user ? (
+          <Login />
+        ) : (
+          <Routes>
+            <Route path="/" element={<Homescreen />} />
+          </Routes>
+        )}
       </Router>
     </div>
   );
