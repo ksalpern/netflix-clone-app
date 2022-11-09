7a9ce70946209b7561c0b11afe907cc0e47d8e3f
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-09 10:04:43 +0200

    add onClick handlers

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -2,9 +2,33 @@ import React from "react";
 import "./SignIn.css";
 
 function SignIn() {
-  return <div className="sign">
-    
-  </div>;
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="sign">
+      <form>
+        <h1>Sign In</h1>
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button onClick={signIn} type="submit">
+          Sign In
+        </button>
+
+        <h4>
+          <span className="sign__gray"> New to Netflix? </span>
+          <span onClick={register} className="sign__link">
+            Sign Up now.
+          </span>
+        </h4>
+      </form>
+    </div>
+  );
 }
 
 export default SignIn;
