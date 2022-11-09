e4b7af7fb2f6c2c2be57c303b25dd74624a98dd7
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-09 13:16:13 +0200

    add navigation to Profile

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Nav.css";
 
 function Nav() {
   const [show, handleShow] = useState(false);
+  const histoty = useNavigate();
 
   const transitionNav = () => {
     if (window.scrollY > 100) {
@@ -26,6 +28,7 @@ function Nav() {
           alt=""
         />
         <img
+          onClick={() => histoty("/profile")}
           className="nav__avatar"
           src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
           alt=""
