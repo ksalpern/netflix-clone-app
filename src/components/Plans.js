09a1dd591aed146164f58d0290943551683d734a
Ksenia PI <ksenia.pidopryhora@gmail.com>
2022-11-10 09:43:52 +0200

    update profile info

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from "react";
 import db from "../firebase";
 import "./Plans.css";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import { loadStripe } from "@stripe/stripe-js";
 
 function Plans() {
   const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    db.collection("customers")
+      .doc(user.uid)
+      .collection("subscriptions")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+          });
+        });
+      });
+  }, [user.uid]);
 
   useEffect(() => {
     db.collection("products")
@@ -26,8 +48,80 @@ function Plans() {
   }, []);
 
   console.log(products);
+  console.log(subscription);
 
-  return <div className="plans"></div>;
+  const loadCheckout = async (priceId) => {
+    const docRef = await db
+      .collection("customers")
+      .doc(user.uid)
+      .collection("checkout_sessions")
+      .add({
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
+
+    docRef.onSnapshot(async (snap) => {
+      const { error, sessionId } = snap.data();
+
+      if (error) {
+        //Show an error to customer and inspect Cloud Func logs in Firebase console
+        alert(`An error accured: ${error.message}`);
+      }
+
+      if (sessionId) {
+        //We have a session, let's redirect to Checkout
+        //Init Stripe
+
+        const stripe = await loadStripe(
+          "pk_test_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+        );
+        stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
+
+  return (
+    <div className="plans">
+      {subscription && (
+        <p>
+          Renewal date:{" "}
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
+      {Object.entries(products).map(([productId, productData]) => {
+        // check if user subscription is active
+
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
+
+        return (
+          <div
+            key={productId}
+            className={`${
+              isCurrentPackage && "plans__plan--disabled"
+            } plans__plan`}
+          >
+            <div className="plans__info">
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Package" : "Subscribe"}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Plans;
